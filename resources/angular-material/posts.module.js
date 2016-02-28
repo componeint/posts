eb57b1a7a2592cf676ec571f88e76cd86bc6db97
anonymoussc <50c5ac69@opayq.com>
2016-02-28 08:45:48 +0700

    New posts.module

---
+++ resources/angular-material/posts.module.js
@@ -0,0 +1,18 @@
+/**
+ * posts.module.js
+ * Created by anonymous on 28/02/16 8:43.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('posts', [
+
+            /* Shared */
+            'appFoundation',
+            'widgets'
+
+        ]);
+
+})();
