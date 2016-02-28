cdd3cecf50aa41aa85a6d1faaa71568800673340
anonymoussc <50c5ac69@opayq.com>
2016-02-29 04:32:08 +0700

    Updated ServiceProvider

--- src/Posts/PostsServiceProvider.php
+++ src/Posts/PostsServiceProvider.php
@@ -38,6 +38,7 @@ class PostsServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->register(\Onderdelen\AppFoundation\AppFoundationServiceProvider::class);
+        $this->app->register(\Onderdelen\Seneschal\SeneschalServiceProvider::class);
         $this->app->register(\Onderdelen\Dashboard\DashboardServiceProvider::class);
     }
 
