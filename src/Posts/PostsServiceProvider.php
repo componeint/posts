0952908dd69360eb16e90043e890eb191ea86055
anonymoussc <50c5ac69@opayq.com>
2016-03-10 08:58:53 +0700

    Modified PostsServiceProvider

--- src/Posts/PostsServiceProvider.php
+++ src/Posts/PostsServiceProvider.php
@@ -37,9 +37,7 @@ class PostsServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(\Componeint\AppFoundation\AppFoundationServiceProvider::class);
-        $this->app->register(\Componeint\Seneschal\SeneschalServiceProvider::class);
-        $this->app->register(\Componeint\Dashboard\DashboardServiceProvider::class);
+        //
     }
 
     /**
